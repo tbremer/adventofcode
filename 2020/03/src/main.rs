e107cdf2e31102bea50a74ce5c96a3e261e3ead9
Tom Bremer <tom@tbremer.com>
2020-12-03 10:05:58 -0600

    day-03 pt2

--- 2020/03/src/main.rs
+++ 2020/03/src/main.rs
@@ -11,10 +11,11 @@ enum GPSError {
 }
 
 type GeologyMap = Vec<Vec<Geology>>;
+type Coords = (usize, usize);
 
 #[derive(Debug)]
 struct GPS {
-    coords: (usize, usize),
+    coords: Coords,
     map: GeologyMap,
 }
 
@@ -24,6 +25,10 @@ impl GPS {
             coords: (0, 0),
             map: geo,
         }
+    }
+
+    pub fn reset_location(&mut self) {
+        self.coords = (0, 0);
     }
 
     pub fn peek_geo(&self) -> &Geology {
@@ -56,6 +61,23 @@ impl GPS {
 
         self.coords.0 = new_idx;
     }
+
+    pub fn move_until_espcaped(&mut self, steps: Coords) -> usize {
+        let mut collision_count = 0;
+
+        loop {
+            self.move_right(steps.0);
+            match self.move_down(steps.1) {
+                Err(_) => break,
+                Ok(_) => (),
+            }
+            if self.peek_geo() == &Geology::Tree {
+                collision_count += 1
+            }
+        }
+
+        collision_count
+    }
 }
 
 fn row_to_geology(row: &str) -> Vec<Geology> {
@@ -69,24 +91,27 @@ fn row_to_geology(row: &str) -> Vec<Geology> {
 }
 
 fn main() {
-    let input: Vec<Vec<Geology>> = utils::str_to_vec(utils::read_file(utils::args().remove(0)))
+    let input: GeologyMap = utils::str_to_vec(utils::read_file(utils::args().remove(0)))
         .iter()
         .map(|r| row_to_geology(r))
         .collect();
     let mut map = GPS::new(input.clone());
-    let mut collision_count = 0;
 
-    loop {
-        map.move_right(3);
-        match map.move_down(1) {
-            Err(_) => break,
-            Ok(_) => (),
-        }
+    // pt 1
+    let r1 = map.move_until_espcaped((3, 1));
+    map.reset_location();
+    println!("pt1 collision_count: {}", r1);
 
-        if map.peek_geo() == &Geology::Tree {
-            collision_count += 1
-        }
-    }
+    // pt 2
+    let r2 = map.move_until_espcaped((1, 1)); //Right 1, down 1.
+    map.reset_location(); // Right 3, down 1. (This is the slope you already checked.)
+    let r3 = map.move_until_espcaped((5, 1)); // Right 5, down 1.
+    map.reset_location();
+    let r4 = map.move_until_espcaped((7, 1)); // Right 7, down 1.
+    map.reset_location();
+    let r5 = map.move_until_espcaped((1, 2)); // Right 1, down 2.
+    map.reset_location();
 
-    println!("pt1 collision_count: {}", collision_count);
+    println!("pt2 collisions: {},{},{},{},{}", r1, r2, r3, r4, r5);
+    println!("pt 2 maths: {}", r1 * r2 * r3 * r4 * r5);
 }
