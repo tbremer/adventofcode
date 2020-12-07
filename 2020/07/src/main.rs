6b1f9897bbee9e583e21f7a9256ed84d1485989d
Tom Bremer <tom@tbremer.com>
2020-12-07 04:07:31 -0600

    day 07 - back out of directed graph solution

--- 2020/07/src/main.rs
+++ 2020/07/src/main.rs
@@ -7,11 +7,6 @@ fn main() {
     let set = find_all_containers("shiny gold", &raw_rules);
 
     println!("pt1: {}", set.len());
-
-    let graph = directed_graph("shiny gold", &raw_rules);
-
-    println!("pt2 graph: {:#?}", graph);
-    println!("pt2 count: {}", count_graph(*graph.child));
 }
 
 type CountRule = (usize, String);
@@ -77,74 +72,4 @@ fn find_all_containers(color: &str, rule_set: &Vec<Rule>) -> HashSet<(usize, Str
     }
 
     can_hold
-}
-
-#[derive(Debug, Clone)]
-struct LinkedListIsh {
-    node: String,
-    count: usize,
-    child: Box<Option<LinkedListIsh>>,
-    sibling: Box<Option<LinkedListIsh>>,
-}
-
-impl Default for LinkedListIsh {
-    fn default() -> Self {
-        LinkedListIsh {
-            node: String::new(),
-            count: 1,
-            child: Box::new(None),
-            sibling: Box::new(None),
-        }
-    }
-}
-
-fn directed_graph(color: &str, rule_set: &Vec<Rule>) -> LinkedListIsh {
-    let mut link = LinkedListIsh::default();
-    for (name, children) in rule_set.iter() {
-        if name != color {
-            continue;
-        }
-
-        link.node = name.to_string();
-
-        link.child = match children {
-            None => Box::new(None),
-            Some(kid_rules) => {
-                let mut kids = kid_rules.iter();
-                let (count, name) = kids.next().unwrap();
-                let mut graph = directed_graph(name, rule_set);
-
-                graph.count = count.clone();
-                graph.sibling = match kids.next() {
-                    None => Box::new(None),
-                    Some((count, name)) => {
-                        let mut sib_graph = directed_graph(name, rule_set);
-                        sib_graph.count = count.clone();
-
-                        Box::new(Some(sib_graph))
-                    }
-                };
-
-                Box::new(Some(graph))
-            }
-        };
-    }
-
-    link
-}
-
-fn count_graph(graph: Option<LinkedListIsh>) -> usize {
-    match graph {
-        None => 0,
-        Some(graph) => {
-            println!(
-                "{}: {}",
-                graph.node,
-                graph.count
-                    + graph.count * count_graph(*graph.child.clone())
-                    + count_graph(*graph.sibling.clone())
-            );
-            graph.count + graph.count * count_graph(*graph.child) + count_graph(*graph.sibling)
-        }
-    }
 }
