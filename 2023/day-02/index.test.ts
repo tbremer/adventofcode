2a22e181b6241ed6674891d1f810d7868ec30837
Tom Bremer <tom@tbremer.com>
2023-12-06 13:07:25 -0600

    day-02

---
+++ 2023/day-02/index.test.ts
@@ -0,0 +1,26 @@
+import { describe, expect, test } from 'bun:test';
+import { pt1, pt2 } from './index';
+
+describe('day-01', () => {
+  test('part 1', () => {
+    const input = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
+    const expected = 8;
+
+    expect(pt1(input)).toBe(expected);
+  });
+
+  test('part 2', () => {
+    const input = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
+    const expected = 2286;
+
+    expect(pt2(input)).toBe(expected);
+  });
+});
---
+++ 2023/day-02/index.ts
@@ -0,0 +1,112 @@
+import { asLines } from '../utils/string';
+
+export function pt1(input: string | string[]): number {
+  const max = {
+    red: 12,
+    green: 13,
+    blue: 14,
+  };
+  const lines = asLines(input);
+  const parsed = lines.map(parseLine);
+
+  return parsed.reduce((acc, cur) => {
+    if (
+      cur.rounds.every(
+        (round) =>
+          round.red <= max.red &&
+          round.green <= max.green &&
+          round.blue <= max.blue,
+      )
+    ) {
+      return acc + cur.id;
+    }
+
+    return acc;
+  }, 0);
+}
+
+export function pt2(input: string): number {
+  const lines = asLines(input);
+  const parsed = lines.map(parseLine);
+
+  const maxPerGame = parsed.map((g) => {
+    const max = g.rounds.reduce(
+      (acc, cur) => {
+        if (cur.red > acc.red) {
+          acc.red = cur.red;
+        }
+
+        if (cur.green > acc.green) {
+          acc.green = cur.green;
+        }
+
+        if (cur.blue > acc.blue) {
+          acc.blue = cur.blue;
+        }
+
+        return acc;
+      },
+      {
+        red: 0,
+        green: 0,
+        blue: 0,
+      },
+    );
+
+    return { id: g.id, max };
+  });
+
+  return maxPerGame.reduce((acc, cur) => {
+    return acc + cur.max.red * cur.max.green * cur.max.blue;
+  }, 0);
+}
+
+const gameRegex = /game (\d+):/i;
+
+function parseLine(line: string): {
+  id: number;
+  rounds: {
+    red: number;
+    green: number;
+    blue: number;
+  }[];
+} {
+  const gameMatcher = line.match(gameRegex);
+  const gameId = gameMatcher ? gameMatcher[1] : -1;
+  const rounds = line
+    .replace(gameRegex, '')
+    .split(';')
+    .map((round) => {
+      const colors = round.split(',').map((c) => c.trim());
+      return colors.reduce(
+        (acc, cur) => {
+          const [count, color] = cur.split(' ');
+          const valid = ensureColorName(color);
+
+          acc[valid] = Number(count);
+
+          return acc;
+        },
+        {
+          red: 0,
+          green: 0,
+          blue: 0,
+        },
+      );
+    });
+
+  return {
+    id: Number(gameId),
+    rounds,
+  };
+}
+
+type Color = 'red' | 'green' | 'blue';
+
+function ensureColorName(color: string): Color {
+  if (color === 'red' || color === 'green' || color === 'blue') {
+    return color;
+  }
+
+  throw new Error(`Invalid color: ${color}`);
+}
